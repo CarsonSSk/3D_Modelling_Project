b4106b4db13f90ad3f9ef947eb7e5ce6fecdacf9
CarsonSSk <carson_skumar@hotmail.com>
2024-07-11 04:44:36 -0400

    Added first version of Assignment 1 code and other deliverables

---
+++ Sprint1/Part1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+// Function prototypes
+int* createArray(int size);
+void initializeArray(int* array, int size);
+void printArray(int* array, int size);
+void deleteArray(int* array);
+
+int main() {
+    int size;
+    
+    std::cout << "Enter the size of the array: ";
+    std::cin >> size;
+    
+    // Create a dynamic array
+    int* array = createArray(size);
+    
+    // Initialize the array
+    initializeArray(array, size);
+    
+    // Print the array
+    printArray(array, size);
+    
+    // Delete the array
+    deleteArray(array);
+
+    std::cout << "Array Deleted!";
+    
+    return 0;
+}
+
+// Function implementations
+int* createArray(int size) {
+    int* array = new int[size];
+    return array;
+}
+
+void initializeArray(int* array, int size) {
+    for (int i = 0; i < size; ++i) {
+        array[i] = i;
+    }
+}
+
+void printArray(int* array, int size) {
+    for (int i = 0; i < size; ++i) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void deleteArray(int* array) {
+    delete[] array;
+}
+
