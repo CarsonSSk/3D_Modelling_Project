44fe353b1637738074d674873fee6fc4967357c7
Carson Senthilkumar <77632579+CarsonSSk@users.noreply.github.com>
2024-07-30 13:59:08 -0400

    Update vertex_shader.glsl
    added aspect ratio to fix rotation issues

--- vertex_shader.glsl
+++ vertex_shader.glsl
@@ -1,6 +1,10 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
+layout(location = 0) in vec3 aPos;
 uniform mat4 transform;
+uniform float aspectRatio;
+
 void main() {
-    gl_Position = transform * vec4(aPos, 1.0);
+    vec3 scaledPos = aPos;
+    scaledPos.y /= aspectRatio;
+    gl_Position = transform * vec4(scaledPos, 1.0);
 }
